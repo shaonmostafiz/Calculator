b0726fe25ca90f68683007212b362c6928b5918b
unknown <shaon.iit08@gmail.com>
2014-02-15 13:10:39 +0600

    make calculator with static mathod call from button

--- src/com/creative/calculator/MainActivity.java
+++ src/com/creative/calculator/MainActivity.java
@@ -1,22 +1,76 @@
 package com.creative.calculator;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	EditText etOp1, etOp2;
+	TextView txtResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		etOp1 = (EditText) findViewById(R.id.etOperad1);
+		etOp2 = (EditText) findViewById(R.id.etOperad2);
+		txtResult = (TextView) findViewById(R.id.tvOutput);
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void add(View v) {
+		String op1Str = etOp1.getText().toString();
+		String op2Str = etOp2.getText().toString();
+		if (op1Str.length() > 0 && op2Str.length() > 0) {
+			double operand1 = Double.parseDouble(op1Str);
+			double operand2 = Double.parseDouble(op2Str);
+			double result = operand1 + operand2;
+			txtResult.setText(op1Str + " + " + op2Str + " = " + result);
+		} else {
+			txtResult.setText("Please Input both the operands");
+		}
+	}
+
+	public void subtract(View v) {
+		String op1Str = etOp1.getText().toString();
+		String op2Str = etOp2.getText().toString();
+		if (op1Str.length() > 0 && op2Str.length() > 0) {
+			double operand1 = Double.parseDouble(op1Str);
+			double operand2 = Double.parseDouble(op2Str);
+			double result = operand1 - operand2;
+			txtResult.setText(op1Str + " - " + op2Str + " = " + result);
+		} else {
+			txtResult.setText("Please Input both the operands");
+		}
+	}
+
+	public void multiply(View v) {
+		String op1Str = etOp1.getText().toString();
+		String op2Str = etOp2.getText().toString();
+		if (op1Str.length() > 0 && op2Str.length() > 0) {
+			double operand1 = Double.parseDouble(op1Str);
+			double operand2 = Double.parseDouble(op2Str);
+			double result = operand1 * operand2;
+			txtResult.setText(op1Str + " * " + op2Str + " = " + result);
+		} else {
+			txtResult.setText("Please Input both the operands");
+		}
+	}
+
+	public void devide(View v) {
+		String op1Str = etOp1.getText().toString();
+		String op2Str = etOp2.getText().toString();
+		if (op1Str.length() > 0 && op2Str.length() > 0) {
+			double operand1 = Double.parseDouble(op1Str);
+			double operand2 = Double.parseDouble(op2Str);
+			double result = operand1 / operand2;
+			txtResult.setText(op1Str + " / " + op2Str + " = " + result);
+		} else {
+			txtResult.setText("Please Input both the operands");
+		}
 	}
 
 }
