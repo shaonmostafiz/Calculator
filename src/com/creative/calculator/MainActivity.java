1cdcaa1d824bc45589606bd0142184d405cb5049
unknown <shaon.iit08@gmail.com>
2014-02-15 16:14:48 +0600

    starting to use method on button click listener

--- src/com/creative/calculator/MainActivity.java
+++ src/com/creative/calculator/MainActivity.java
@@ -2,14 +2,16 @@ package com.creative.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	EditText etOp1, etOp2;
 	TextView txtResult;
+	Button btnPlus, btnMinus, btnMultiply, btnDevide;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,59 +20,64 @@ public class MainActivity extends Activity {
 		etOp1 = (EditText) findViewById(R.id.etOperad1);
 		etOp2 = (EditText) findViewById(R.id.etOperad2);
 		txtResult = (TextView) findViewById(R.id.tvOutput);
-
+		btnPlus = (Button) findViewById(R.id.btnPlus);
+		btnMinus = (Button) findViewById(R.id.btnMinus);
+		btnMultiply = (Button) findViewById(R.id.btnMultiplication);
+		btnDevide = (Button) findViewById(R.id.btnDivision);
+		
+		btnPlus.setOnClickListener(l);
 	}
 
-	public void add(View v) {
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		if (op1Str.length() > 0 && op2Str.length() > 0) {
-			double operand1 = Double.parseDouble(op1Str);
-			double operand2 = Double.parseDouble(op2Str);
-			double result = operand1 + operand2;
-			txtResult.setText(op1Str + " + " + op2Str + " = " + result);
-		} else {
-			txtResult.setText("Please Input both the operands");
-		}
-	}
-
-	public void subtract(View v) {
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		if (op1Str.length() > 0 && op2Str.length() > 0) {
-			double operand1 = Double.parseDouble(op1Str);
-			double operand2 = Double.parseDouble(op2Str);
-			double result = operand1 - operand2;
-			txtResult.setText(op1Str + " - " + op2Str + " = " + result);
-		} else {
-			txtResult.setText("Please Input both the operands");
-		}
-	}
-
-	public void multiply(View v) {
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		if (op1Str.length() > 0 && op2Str.length() > 0) {
-			double operand1 = Double.parseDouble(op1Str);
-			double operand2 = Double.parseDouble(op2Str);
-			double result = operand1 * operand2;
-			txtResult.setText(op1Str + " * " + op2Str + " = " + result);
-		} else {
-			txtResult.setText("Please Input both the operands");
-		}
-	}
-
-	public void devide(View v) {
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		if (op1Str.length() > 0 && op2Str.length() > 0) {
-			double operand1 = Double.parseDouble(op1Str);
-			double operand2 = Double.parseDouble(op2Str);
-			double result = operand1 / operand2;
-			txtResult.setText(op1Str + " / " + op2Str + " = " + result);
-		} else {
-			txtResult.setText("Please Input both the operands");
-		}
-	}
+//	public void add(View v) {
+//		String op1Str = etOp1.getText().toString();
+//		String op2Str = etOp2.getText().toString();
+//		if (op1Str.length() > 0 && op2Str.length() > 0) {
+//			double operand1 = Double.parseDouble(op1Str);
+//			double operand2 = Double.parseDouble(op2Str);
+//			double result = operand1 + operand2;
+//			txtResult.setText(op1Str + " + " + op2Str + " = " + result);
+//		} else {
+//			txtResult.setText("Please Input both the operands");
+//		}
+//	}
+//
+//	public void subtract(View v) {
+//		String op1Str = etOp1.getText().toString();
+//		String op2Str = etOp2.getText().toString();
+//		if (op1Str.length() > 0 && op2Str.length() > 0) {
+//			double operand1 = Double.parseDouble(op1Str);
+//			double operand2 = Double.parseDouble(op2Str);
+//			double result = operand1 - operand2;
+//			txtResult.setText(op1Str + " - " + op2Str + " = " + result);
+//		} else {
+//			txtResult.setText("Please Input both the operands");
+//		}
+//	}
+//
+//	public void multiply(View v) {
+//		String op1Str = etOp1.getText().toString();
+//		String op2Str = etOp2.getText().toString();
+//		if (op1Str.length() > 0 && op2Str.length() > 0) {
+//			double operand1 = Double.parseDouble(op1Str);
+//			double operand2 = Double.parseDouble(op2Str);
+//			double result = operand1 * operand2;
+//			txtResult.setText(op1Str + " * " + op2Str + " = " + result);
+//		} else {
+//			txtResult.setText("Please Input both the operands");
+//		}
+//	}
+//
+//	public void devide(View v) {
+//		String op1Str = etOp1.getText().toString();
+//		String op2Str = etOp2.getText().toString();
+//		if (op1Str.length() > 0 && op2Str.length() > 0) {
+//			double operand1 = Double.parseDouble(op1Str);
+//			double operand2 = Double.parseDouble(op2Str);
+//			double result = operand1 / operand2;
+//			txtResult.setText(op1Str + " / " + op2Str + " = " + result);
+//		} else {
+//			txtResult.setText("Please Input both the operands");
+//		}
+//	}
 
 }
